b02c349279f83ab9b143f20634d3284ad1369e3e
Casey Watts <casey.s.watts@gmail.com>
2020-02-02 18:14:57 -0500

    move text to right interact text

--- src/lib/displayInteractText.js
+++ src/lib/displayInteractText.js
@@ -4,7 +4,7 @@ import configText from '../lib/configText';
 export default function displayInteractText(scene, player){
 
     var interactTextContainer = scene.add.container(player.x, player.y);
-    var interactText = configText(scene.add.text(300,0,'Press SPACE to interact'), interactTextContainer, '#000', '#333333');
+    var interactText = configText(scene.add.text(600,0,'Press SPACE to interact'), interactTextContainer, '#000', '#333333');
     setTimeout(() => {interactText.destroy(true)}, 10); 
 
 }
