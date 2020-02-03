d1c33d2cd247f452df58776474c81a92efb3c686
Casey Watts <casey.s.watts@gmail.com>
2020-02-02 23:34:10 -0500

    cleanup

--- src/lib/setupPlayerMovement.js
+++ src/lib/setupPlayerMovement.js
@@ -1,5 +1,4 @@
 export default function setupMovement (scene, player) {
-    const walkingSounds = player.walkingSounds;
     // Creates object for input with arrow keys
     const moveKeys = scene.input.keyboard.addKeys({
         'up': Phaser.Input.Keyboard.KeyCodes.UP,
@@ -34,41 +33,4 @@ export default function setupMovement (scene, player) {
     scene.input.keyboard.on('keyup_RIGHT', (event) => {
         player.updateMovement(moveKeys)
     });
-
-
-    // const allKeysAreUp = function () { return moveKeys['up'].isUp && moveKeys['down'].isUp && moveKeys['left'].isUp && moveKeys['right'].isUp; }
-
-    // // Stops player acceleration on uppress of WASD keys
-    // scene.input.keyboard.on('keyup_UP', (event) => {
-    //     if (moveKeys['down'].isUp) {
-    //         player.setVelocityY(0);
-    //     }
-    //     if (allKeysAreUp()) {
-    //         player.stop()
-    //     }
-    // });
-    // scene.input.keyboard.on('keyup_DOWN', (event) => {
-    //     if (moveKeys['up'].isUp) {
-    //         player.setVelocityY(0);
-    //     }
-    //     if (allKeysAreUp()) {
-    //         player.stop()
-    //     }
-    // });
-    // scene.input.keyboard.on('keyup_LEFT', (event) => {
-    //     if (moveKeys['right'].isUp) {
-    //         player.setVelocityX(0);
-    //     }
-    //     if (allKeysAreUp()) {
-    //         player.stop()
-    //     }
-    // });
-    // scene.input.keyboard.on('keyup_RIGHT', (event) => {
-    //     if (moveKeys['left'].isUp) {
-    //         player.setVelocityX(0);
-    //     }
-    //     if (allKeysAreUp()) {
-    //         player.stop()
-    //     }
-    // });
 }
